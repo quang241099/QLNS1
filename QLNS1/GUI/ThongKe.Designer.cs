d1830c447c329c80085c49469ce95c652cac0b0a
TrieuThuyHang <trieuthuyhang2008@gmail.com>
2021-08-27 16:49:14 +0700

    thong ke so bo

--- QLNS1/GUI/ThongKe.Designer.cs
+++ QLNS1/GUI/ThongKe.Designer.cs
@@ -44,6 +44,21 @@ namespace QLNS1.GUI
                this.emptySpaceItem1 = new DevExpress.XtraLayout.EmptySpaceItem();
                this.btnTK = new DevExpress.XtraEditors.SimpleButton();
                this.labelControl1 = new DevExpress.XtraEditors.LabelControl();
+               this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+               this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+               this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+               this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+               this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+               this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+               this.Column7 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+               this.Column8 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+               this.Column9 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+               this.Column10 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+               this.Column11 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+               this.Column12 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+               this.Column13 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+               this.Column14 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+               this.Column15 = new System.Windows.Forms.DataGridViewTextBoxColumn();
                ((System.ComponentModel.ISupportInitialize)(this.comboBoxEdit1.Properties)).BeginInit();
                ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
                ((System.ComponentModel.ISupportInitialize)(this.chartControl1)).BeginInit();
@@ -69,6 +84,22 @@ namespace QLNS1.GUI
                // dataGridView1
                // 
                this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+               this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column5,
+            this.Column6,
+            this.Column7,
+            this.Column8,
+            this.Column9,
+            this.Column10,
+            this.Column11,
+            this.Column12,
+            this.Column13,
+            this.Column14,
+            this.Column15});
                this.dataGridView1.Location = new System.Drawing.Point(37, 95);
                this.dataGridView1.Name = "dataGridView1";
                this.dataGridView1.RowHeadersWidth = 51;
@@ -165,6 +196,111 @@ namespace QLNS1.GUI
                this.labelControl1.TabIndex = 6;
                this.labelControl1.Text = "Thống kê theo";
                // 
+               // Column1
+               // 
+               this.Column1.HeaderText = "Mã nhân viên";
+               this.Column1.MinimumWidth = 6;
+               this.Column1.Name = "Column1";
+               this.Column1.Width = 125;
+               // 
+               // Column2
+               // 
+               this.Column2.HeaderText = "Mã HTTD";
+               this.Column2.MinimumWidth = 6;
+               this.Column2.Name = "Column2";
+               this.Column2.Width = 125;
+               // 
+               // Column3
+               // 
+               this.Column3.HeaderText = "Mã chức vụ";
+               this.Column3.MinimumWidth = 6;
+               this.Column3.Name = "Column3";
+               this.Column3.Width = 125;
+               // 
+               // Column4
+               // 
+               this.Column4.HeaderText = "Mã phòng ban";
+               this.Column4.MinimumWidth = 6;
+               this.Column4.Name = "Column4";
+               this.Column4.Width = 125;
+               // 
+               // Column5
+               // 
+               this.Column5.HeaderText = "Nơi làm việc";
+               this.Column5.MinimumWidth = 6;
+               this.Column5.Name = "Column5";
+               this.Column5.Width = 125;
+               // 
+               // Column6
+               // 
+               this.Column6.HeaderText = "Họ tên";
+               this.Column6.MinimumWidth = 6;
+               this.Column6.Name = "Column6";
+               this.Column6.Width = 125;
+               // 
+               // Column7
+               // 
+               this.Column7.HeaderText = "Ngày sinh";
+               this.Column7.MinimumWidth = 6;
+               this.Column7.Name = "Column7";
+               this.Column7.Width = 125;
+               // 
+               // Column8
+               // 
+               this.Column8.HeaderText = "Quê quán";
+               this.Column8.MinimumWidth = 6;
+               this.Column8.Name = "Column8";
+               this.Column8.Width = 125;
+               // 
+               // Column9
+               // 
+               this.Column9.HeaderText = "Giới tính";
+               this.Column9.MinimumWidth = 6;
+               this.Column9.Name = "Column9";
+               this.Column9.Width = 125;
+               // 
+               // Column10
+               // 
+               this.Column10.HeaderText = "Số CMND";
+               this.Column10.MinimumWidth = 6;
+               this.Column10.Name = "Column10";
+               this.Column10.Width = 125;
+               // 
+               // Column11
+               // 
+               this.Column11.HeaderText = "Trình độ";
+               this.Column11.MinimumWidth = 6;
+               this.Column11.Name = "Column11";
+               this.Column11.Width = 125;
+               // 
+               // Column12
+               // 
+               this.Column12.HeaderText = "SĐT";
+               this.Column12.MinimumWidth = 6;
+               this.Column12.Name = "Column12";
+               this.Column12.Width = 125;
+               // 
+               // Column13
+               // 
+               this.Column13.HeaderText = "Email";
+               this.Column13.MinimumWidth = 6;
+               this.Column13.Name = "Column13";
+               this.Column13.Width = 125;
+               // 
+               // Column14
+               // 
+               this.Column14.HeaderText = "Nơi ở";
+               this.Column14.MinimumWidth = 6;
+               this.Column14.Name = "Column14";
+               this.Column14.Width = 125;
+               // 
+               // Column15
+               // 
+               this.Column15.HeaderText = "Nơi sinh";
+               this.Column15.MinimumWidth = 6;
+               this.Column15.Name = "Column15";
+               this.Column15.Width = 125;
+               // 
                // ThongKe
                // 
                this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
@@ -210,5 +346,20 @@ namespace QLNS1.GUI
           private EmptySpaceItem emptySpaceItem1;
           private SimpleButton btnTK;
           private LabelControl labelControl1;
+          private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+          private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+          private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+          private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+          private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+          private System.Windows.Forms.DataGridViewTextBoxColumn Column6;
+          private System.Windows.Forms.DataGridViewTextBoxColumn Column7;
+          private System.Windows.Forms.DataGridViewTextBoxColumn Column8;
+          private System.Windows.Forms.DataGridViewTextBoxColumn Column9;
+          private System.Windows.Forms.DataGridViewTextBoxColumn Column10;
+          private System.Windows.Forms.DataGridViewTextBoxColumn Column11;
+          private System.Windows.Forms.DataGridViewTextBoxColumn Column12;
+          private System.Windows.Forms.DataGridViewTextBoxColumn Column13;
+          private System.Windows.Forms.DataGridViewTextBoxColumn Column14;
+          private System.Windows.Forms.DataGridViewTextBoxColumn Column15;
      }
 }
